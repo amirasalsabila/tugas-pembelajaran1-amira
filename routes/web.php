a8355751b4c7429a9e9240ceeb6ad4399ee3a7e4
amirasalsabila <amirasalsabila2005@gmail.com>
2025-10-06 21:43:17 +0700

    Tugas Pembelajaran 3

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('home/welcome);
-    return 'Hallo Laravel';
+    return view ('index');
 });
 
 Route::get('about', function () {
-    return 'Ini adalah halaman <b>about</b>';
+    return view ('about');
 });
 
 Route::get('mahasiswa', function () {
-    return 'Ini adalah halaman <b>mahasiswa</b>';
+    return view ('mahasiswa');
 });
 
 Route::get('profile', function () {
-    $nama = 'AMIRA';
-    return 'Ini adalah halaman <b>profile</b>, nama saya adalah '. $nama;
+    $nama = 'AMIRA SALSABILA';
+    //return view ('profile', compact('nama'));
+    return view ('profile')->with('nama', $nama);
 });
